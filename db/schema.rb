6b7805871afbc1c60a5fe19c48ada080847e9443
Caleb Schoepp <caleb.schoepp@gmail.com>
2022-01-15 13:41:55 -0700

    Associate content with users. Wheee were doing it

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_15_203052) do
+ActiveRecord::Schema.define(version: 2022_01_15_204040) do
 
   create_table "contents", force: :cascade do |t|
     t.text "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_contents_on_user_id"
   end
 
   create_table "questions", force: :cascade do |t|
@@ -43,5 +45,6 @@ ActiveRecord::Schema.define(version: 2022_01_15_203052) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "contents", "users"
   add_foreign_key "questions", "contents"
 end
