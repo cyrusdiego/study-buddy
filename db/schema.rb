46a364d64d20b47802ea3d0c251048eb6cdb107a
Caleb Schoepp <caleb.schoepp@gmail.com>
2022-01-15 13:35:49 -0700

    QUESTIONS. I have so many questions

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_15_202354) do
+ActiveRecord::Schema.define(version: 2022_01_15_203052) do
 
   create_table "contents", force: :cascade do |t|
     t.text "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.text "question"
+    t.integer "content_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["content_id"], name: "index_questions_on_content_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +43,5 @@ ActiveRecord::Schema.define(version: 2022_01_15_202354) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "questions", "contents"
 end
