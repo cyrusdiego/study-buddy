7368a3b618e28c6d2484e1f3ba2195deb95e29fb
Caleb Schoepp <caleb.schoepp@gmail.com>
2022-01-15 13:25:56 -0700

    Hooray, content is everywhere. EVERYWHERE!

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_15_201923) do
+ActiveRecord::Schema.define(version: 2022_01_15_202354) do
+
+  create_table "contents", force: :cascade do |t|
+    t.text "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
