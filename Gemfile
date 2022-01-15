4c3377e218db198586b5b801017612f093f192f5
Caleb Schoepp <caleb.schoepp@gmail.com>
2022-01-15 16:55:54 -0700

    I couldn't think of a funny commit name
    * Setup content with attachment
    
    * make some changes
    
    * A bunch of styling

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'turbolinks', '5.2.1'
 gem 'jbuilder',   '2.10.0'
 gem 'bootsnap',   '1.7.2', require: false
 gem 'devise'
+gem 'active_storage_validations'
 
 group :development, :test do
   gem 'sqlite3', '1.4.2'
--- app/models/content.rb
+++ app/models/content.rb
@@ -1,4 +1,7 @@
 class Content < ApplicationRecord
   has_many :questions
   belongs_to :user
+  has_one_attached :file
+  validates :file, attached: true, content_type: ['application/pdf'], size: { less_than: 20.megabytes, message: 'is not given between size' }
+  validates :title, presence: true
 end
