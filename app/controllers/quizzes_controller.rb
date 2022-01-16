663931e24ac706acb51da06a2e31cbdc50570535
Caleb Schoepp <caleb.schoepp@gmail.com>
2022-01-15 21:43:56 -0700

    Yeehaw

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -15,16 +15,15 @@ class QuizzesController < ApplicationController
   # POST /quizzes
   # POST /quizzes.json
   def create
-    # TODO
-    @quiz = Content.new(content_params)
+    @quiz = Quiz.new(quiz_params)
     @quiz.user = current_user
 
     respond_to do |format|
       if @quiz.save
-        format.html { redirect_to @quiz, notice: 'Content was successfully created.' }
+        format.html { redirect_to @quiz, notice: 'Quiz was successfully created.' }
         format.json { render :show, status: :created, location: @quiz }
       else
-        format.html { render :new }
+        format.html { redirect_to contents_url, alert: 'Failed to create quiz.' }
         format.json { render json: @quiz.errors, status: :unprocessable_entity }
       end
     end
@@ -38,6 +37,6 @@ class QuizzesController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def quiz_params
-    params.require(:quiz).permit
+    params.require(:quiz).permit(content_ids: [])
   end
 end
