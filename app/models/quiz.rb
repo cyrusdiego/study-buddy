b8957b23890af39afc16c3aca917d81e5b1d9039
Caleb Schoepp <caleb.schoepp@gmail.com>
2022-01-16 12:22:06 -0700

    Shuffle and correct validate

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -4,11 +4,11 @@ class Quiz < ApplicationRecord
   has_many :tags, through: :quiz_tags
   has_many :contents, through: :quiz_contents
   belongs_to :user
-  validate :must_have_one_content
+  validate :must_have_one_content_or_tag
 
-  def must_have_one_content
-    if self.contents.empty?
-      errors.add(:quiz, 'You must select at least one piece of content')
+  def must_have_one_content_or_tag
+    if self.contents.empty? and self.tags.empty?
+      errors.add(:quiz, 'You must select at least one piece of content or tag')
     end
   end
 end
