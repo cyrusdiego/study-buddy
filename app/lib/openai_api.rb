75d465c1a5a0a2f99fd965df4039e0df9aa69522
Ryan Kortbeek <rakortbe@ualberta.ca>
2022-01-16 02:10:32 -0700

    Implement answer fetch util function and refactor

--- app/lib/openai_api.rb
+++ app/lib/openai_api.rb
@@ -6,33 +6,18 @@ TODOs
 
 - overall
   - make openaiclient private?
-
 - question gen
   - open ai related
     - refine question gen prompt
     - tweak params
-
 - answer gen
   - open ai related
     - create answer gen prompt
     - tweak params
-  - code related
-    - create util functions for answer gen
-    - test question gen util functions
-    - figure out openai response object and build in error handling
 =end
 
-module OpenAiApi
-  class OpenAiClient
-    include Singleton
 
-    attr_reader :client
-    
-    def initialize
-      @client = OpenAI::Client.new(access_token: Rails.application.credentials.openai[:access_token])
-    end
-  end
-
+module OpenAiApiParameters 
   @@question_gen_prompt = "
 Create a list of questions based on the content
 
@@ -79,31 +64,76 @@ Content:
 Questions:
 
 "
-  @@answer_gen_prompt = ""
-  @@max_tokens = 2000 # 2048 but due to approximated token length of 4 rounding down to 2000
-  @@min_completion_tokens = 300
+  def self.question_gen_prompt
+    @@question_gen_prompt
+  end
 
-  def fetch_question_set_array content_array
-    content_array.each do |content| 
-      question_sets.append(self.fetch_question_set content)
+  # Sourced from https://en.wikipedia.org/wiki/Fall_of_the_Western_Roman_Empire#:~:text=The%20fall%20of%20the%20Western,divided%20into%20several%20successor%20polities.
+  @@answer_examples = [
+    ["What caused the fall of the Western Roman Empire?", "The fall of the Western Roman Empire was due to a loss of effective control over its Western provinces."],
+    ["What were some of the immediate factors that contributed to the collapse?", "Some of the immediate factors that contributed to the collapse include a smaller and less effective army, the health and size of the Roman population, the strength of the economy, the competence of the emporers, the internal struggles for power, and changes in religious beliefs. External pressures from invading barbarians, climatic changes, and epidemic disease exaggerated many of these immediate factors."],
+    ["What are some of the major subjects of the historiography of the ancient world?", "The reasons for the collapse of the Western Roman Empire."]
+  ]
+  def self.answer_examples
+    @@answer_examples
+  end
+
+  @@answer_examples_context = "The fall of the Western Roman Empire was the loss of central political control in the Western Roman Empire, a process in which the Empire failed to enforce its rule, and its vast territory was divided into several successor polities. The Roman Empire lost the strengths that had allowed it to exercise effective control over its Western provinces; modern historians posit factors including the effectiveness and numbers of the army, the health and numbers of the Roman population, the strength of the economy, the competence of the emperors, the internal struggles for power, the religious changes of the period, and the efficiency of the civil administration. Increasing pressure from invading barbarians outside Roman culture also contributed greatly to the collapse. Climatic changes and epidemic disease drove many of these immediate factors. The reasons for the collapse are major subjects of the historiography of the ancient world and they inform much modern discourse on state failure."
+  def self.answer_examples_context
+    @@answer_examples_context
+  end
+
+  # Maximum is 2048 but due to approximated token length of 4 rounding down to 2000
+  @@max_tokens = 2000
+  def self.max_tokens
+    @@max_tokens
+  end
+
+  @@min_completion_tokens = 300
+  def self.min_completion_tokens
+    @@min_completion_tokens
+  end
+end
+
+module OpenAiApi
+  include OpenAiApiParameters
+
+  class OpenAiClient
+    include Singleton
+
+    attr_reader :client
+    
+    def initialize
+      @client = OpenAI::Client.new(access_token: Rails.application.credentials.openai[:access_token])
     end
   end
-  module_function :fetch_question_set_array
 
   def fetch_question_set content
-    prompt = self.generate_prompt true, content
+    prompt = self.generate_prompt content
     completion_tokens = self.calculate_completion_tokens prompt
-    self.parse_response(OpenAiClient.instance.client.completions(
+    self.parse_response :question_set, OpenAiClient.instance.client.completions(
       engine: "davinci-instruct-beta-v3", 
       parameters: {
         prompt: prompt,
         max_tokens: completion_tokens
-      }))
+      })
   end
   module_function :fetch_question_set
 
-  def self.generate_prompt is_question_gen, content
-    prompt = is_question_gen ? @@question_gen_prompt % {content: content} : @@answer_gen_prompt
+  def fetch_answer content, question
+    self.parse_response :answer, OpenAiClient.instance.client.answers(parameters: {
+      model: "curie",
+      question: question,
+      examples: @@answer_examples,
+      examples_context: @@answer_examples_context,
+      documents: [content],
+      max_tokens: @@min_completion_tokens
+    })
+  end
+  module_function :fetch_answer
+
+  def self.generate_prompt content
+    prompt = @@question_gen_prompt % {content: content}
     prompt_tokens = self.calculate_prompt_tokens prompt
     if prompt_tokens >= @@max_tokens - @@min_completion_tokens
       raise Exception.new "Content is too large!"
@@ -124,25 +154,42 @@ Questions:
   end
   private_class_method :calculate_completion_tokens
 
-  def self.parse_response response
+  def self.parse_response response_type, response
     if response.code == 200
-      begin
-        response.parsed_response["choices"].map{ |questions| questions["text"].strip.split("\n") }.first
-      rescue Exception => ex
-        raise Exception.new "Unexpected error occured while parsing successful response.\n%{error_info}" % 
-          {error_info: ex.inspect}
-      end
+      self.parse_successful_response response_type, response
     else
-      begin
-        raise Exception.new "%{status_code}: %{error_msg}" % {
-          status_code: response.code, 
-          error_msg: response.parsed_response["error"]["message"]
-        }
-      rescue Exception => ex
-        raise Exception.new "Unexpected error occured while parsing erroneous response.\n%{error_info}" % 
-          {error_info: ex.inspect}
-      end
+      self.handle_erroneous_response response
     end
   end
   private_class_method :parse_response
+
+  def self.parse_successful_response response_type, response
+    begin
+      case response_type
+      when :question_set
+        response.parsed_response["choices"].map{ |questions| questions["text"].strip.split("\n").map{ |question| question.strip }}.first
+      when :answer
+        response.parsed_response["answers"]
+      else
+        raise Exception.new "Invalid response_type passed into OpenAiApi."
+      end
+    rescue Exception => ex
+      raise Exception.new "Unexpected error occured while parsing successful response.\n%{error_info}" % 
+        {error_info: ex.inspect}
+    end
+  end
+  private_class_method :parse_successful_response
+
+  def self.handle_erroneous_response response
+    begin
+      raise Exception.new "%{status_code}: %{error_msg}" % {
+        status_code: response.code, 
+        error_msg: response.parsed_response["error"]["message"]
+      }
+    rescue Exception => ex
+      raise Exception.new "Unexpected error occured while parsing erroneous response.\n"\
+        "Response status code: %{status_code}\n%{error_info}" % {status_code: response.code, error_info: ex.inspect}
+    end
+  end
+  private_class_method :handle_erroneous_response
 end
