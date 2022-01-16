9c5eaf92ae09795ca105af53b5b3806f87737240
Caleb Schoepp <caleb.schoepp@gmail.com>
2022-01-16 02:33:45 -0700

    Caleb/tags data (#40)
    * A start
    
    * Tags

--- app/models/content.rb
+++ app/models/content.rb
@@ -2,6 +2,8 @@ class Content < ApplicationRecord
   has_many :questions, dependent: :destroy
   has_many :quiz_contents, dependent: :destroy
   has_many :quizzes, through: :quiz_contents, dependent: :destroy
+  has_many :content_tags
+  has_many :tags, through: :content_tags
   belongs_to :user
   has_one_attached :file
   validates :file, attached: true, content_type: ['application/pdf'], size: { less_than: 20.megabytes, message: 'is not given between size' }
@@ -9,5 +11,18 @@ class Content < ApplicationRecord
 
   def summary
     return "#{self.title} (#{self.questions.count} questions)"
+  end
+
+  # this is the tag's list setter which will capture
+  # params[:customer][:add_phone_numbers] when you submit the form
+  def add_tags=(tag_string)
+    tag_string.split(" ").each do |tag|
+      self.tags << Tag.new(name: tag) unless tags.map { |t| t.name }.include? tag
+    end
+  end
+
+  # this will display tags in the text_field
+  def add_tags
+    tags.map { |t| t.name }.join(" ")
   end
 end
